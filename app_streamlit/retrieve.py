7ea37faabdb21cc8a37e0dc15b789a4d232afb33
Aznor Yusof <aznor1990@gmail.com>
2023-08-24 18:16:54 +0800

    working api and chain

---
+++ app_streamlit/retrieve.py
@@ -0,0 +1,94 @@
+import os
+import sys
+
+from dotenv import load_dotenv
+from langchain import PromptTemplate
+from langchain.chains import ConversationalRetrievalChain
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.chat_models import ChatOpenAI
+
+load_dotenv()
+
+MAIN_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(MAIN_DIR)
+
+DB_PATH = os.environ.get('DB_PATH')
+EMBEDDING_MODEL = os.environ.get('EMBEDDING_MODEL')
+OPEN_AI_BASE=os.environ.get('OPEN_AI_BASE')
+API_KEY=os.environ.get('API_KEY')
+SEED=int(os.environ.get('SEED'))
+TEMPERATURE=float(os.environ.get('TEMPERATURE'))
+TOP_K=int(os.environ.get('TOP_K'))
+
+
+def _get_chat_history(inputs):
+    # Processes the chat history by combining the responses from 
+    # user and the assistant and returning as a formatted string 
+    res = []
+    for _i in inputs:
+        if _i.get("role") == "user":
+            user_content = _i.get("content")
+        if _i.get("role") == "assistant":
+            assistant_content = _i.get("content")
+            res.append(f"user:{user_content}\nassistant:{assistant_content}")
+    return "\n".join(res)
+
+
+def build_chain():
+    # Define the embedding function used for documents
+    embedding_function = HuggingFaceInstructEmbeddings(
+        model_name=EMBEDDING_MODEL,
+
+    )
+
+    # Instantiate the document vector DB from the path
+    db = Chroma(
+        persist_directory=os.path.join(MAIN_DIR, DB_PATH), 
+        embedding_function=embedding_function
+    )
+    
+    # Instantiate the llm chat client
+    llm = ChatOpenAI(
+        openai_api_key=API_KEY,
+        openai_api_base=OPEN_AI_BASE,
+        temperature=TEMPERATURE
+    )
+
+    # Specify the variables and template to 
+    condense_qa_template = """
+    Given the following conversation and a follow up question, rephrase the follow up question
+    to be a standalone question.
+
+    Chat History:
+    {chat_history}
+    Follow Up Input: {question}
+    Standalone question:"""
+
+    standalone_question_prompt = PromptTemplate.from_template(
+        condense_qa_template
+    )
+    
+    qa = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=db.as_retriever(search_kwargs={"k": TOP_K}),
+        condense_question_prompt=standalone_question_prompt,
+        return_source_documents=True,
+        get_chat_history=_get_chat_history
+    )
+
+    return qa
+
+
+def run_chain(chain, prompt, history):
+    return chain({"question": prompt, "chat_history": history})
+
+
+if __name__ == "__main__":
+    #prompt = "Hi, can you tell me more about Aznor and how he can contribute as an AI Platform Engineer?"
+    prompt = "Hi, give me a summary of Aznor's work experience in the past three years in bullet points. It is now 2023."
+    #prompt = "Hi, can you tell me more about Elon Musk?"
+    #prompt = "Hi, how many world cups have singapore won in football?"
+
+    chain = build_chain()
+    output = run_chain(chain, prompt, [])
---
+++ database/create_db_from_docs.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from langchain.document_loaders import DirectoryLoader
+from langchain.vectorstores import Chroma
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MAIN_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(MAIN_DIR)
+
+DB_PATH = os.environ.get('DB_PATH')
+DOCS_PATH = os.environ.get('DOCS_PATH')
+CHUNK_SIZE = int(os.environ.get('CHUNK_SIZE'))
+CHUNK_OVERLAP = int(os.environ.get('CHUNK_OVERLAP'))
+
+def setup_knowledge_base(docs_dir, db_path, embedding_model):
+    loader = DirectoryLoader(docs_dir, glob="**/*.txt")
+    docs = loader.load()
+    embeddings = HuggingFaceInstructEmbeddings(
+        model_name=embedding_model
+    )
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
+    texts = text_splitter.split_documents(docs)
+    _ = Chroma.from_documents(texts, embeddings, persist_directory=db_path)
+
+
+if __name__ == "__main__":
+    setup_knowledge_base(
+        os.path.join(MAIN_DIR, DOCS_PATH),
+        os.path.join(MAIN_DIR, DB_PATH),
+        "hkunlp/instructor-large"
+    )
